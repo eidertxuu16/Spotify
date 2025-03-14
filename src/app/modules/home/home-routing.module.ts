d745d3b7898a4a6b1770ed1bf839e6d95bb2dd02
eidertxuu16 <eidergp.16@gmail.com>
2025-03-14 09:08:28 +0100

    sidebar implemented

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from 'src/app/shared/components/side-bar/side-bar.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: SideBarComponent
   }
 ];
 
