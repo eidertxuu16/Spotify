9b49b2acacd5045ea0ea339d77dcdacd2e58b83c
eidertxuu16 <eidergp.16@gmail.com>
2025-03-14 09:04:44 +0100

    Shared Module fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    SideBarComponent,
+    HeaderUserComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    SideBarComponent
   ],
   imports: [
     CommonModule
