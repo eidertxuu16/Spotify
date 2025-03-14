b4a55f756cf473d49e6fdb8c98672006cca48c53
eidertxuu16 <eidergp.16@gmail.com>
2025-03-14 08:38:52 +0100

    headerUser created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderUserComponent } from './modules/shared/components/header-user/header-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderUserComponent
   ],
   imports: [
     BrowserModule,
