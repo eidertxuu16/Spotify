5ebb7a12fed47e49920e6699dd55f501c0bae9f5
eidertxuu16 <eidergp.16@gmail.com>
2025-03-14 09:11:16 +0100

    sidebar done

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,6 +6,64 @@ import { Component } from '@angular/core';
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+  mainMenu: {
+    defaultOptions: Array<any>,
+    accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] }
+  customOptions: Array<any> = []
 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Search',
+        icon: 'uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Your Library',
+        icon: 'uil-chart',
+        router: ['/', 'favourites']
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'New Playlist',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Your Favourite Songs',
+        icon: 'uil-heart'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'My Playlist #1',
+        router: ['/']
+      },
+      {
+        name: 'My Playlist #2',
+        router: ['/']
+      },
+      {
+        name: 'My Playlist #3',
+        router: ['/']
+      },
+      {
+        name: 'My Playlist #4',
+        router: ['/']
+      }
+
+    ]
+  }
 }
